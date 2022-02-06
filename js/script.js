1580564aa9af3b48cbef8bdab06682f5a0c2fe32
edah-hub <edah.chepngetich@student.moringaschool.com>
2022-02-06 12:49:26 +0300

    a variable in js declared

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert()
+function mySubmitButton(){
+
+    var score = 0;
+    var currentAnswer1 = document.Questions.question1.value;
+    var currentAnswer2 = document.Questions.question2.value;
+    var currentAnswer3 = document.Questions.question3.value;
+    var currentAnswer4 = document.Questions.question4.value;
+    var currentAnswer5= document.Questions.question5.value;
+    var results=document.getElementByClass("results");
+
+
+
+}
